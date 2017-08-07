62b965d8f1732731a9ad21247e70ac9233829db5
pradeep <pkadnor@mcftech.com>
2017-08-07 11:58:18 +0530

    1.basic practice and implementation with angular js 2.working on more features

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+﻿/*
+@Author Pradeep D Kadnor
+@Date 04/08/2017
+@@ Dashboard for site dynamically fetch from alrefso site 
+*/
+
+'use strict';
+
+var qApp = angular.module('qApp', []);
+
+qApp.controller('qCtrl', function ($scope, $http) {
+
+    $scope.clearSearchbox = function () {
+        $scope.myProfile.search != undefined ? $scope.myProfile.search="" : $scope.myProfile.search//clear search string
+    }
+
+    //read json data
+    $http({ method: 'GET', url: 'siteData.json' }).then(function (response, status) {
+        if (response.status == 200) {
+            $scope.myProfile = response.data;
+        }
+    });
+});
